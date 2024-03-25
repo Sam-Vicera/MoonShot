cbb72dadce3a1dc29c6947c618e921b85d42b7f2
Samuel Hernandez Vicera <shernandezvicera@apple.com>
2024-03-25 12:05:08 -0700

    Initial Commit

---
+++ MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by Samuel Hernandez Vicera on 3/25/24.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
